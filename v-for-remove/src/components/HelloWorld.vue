20a44d274b74370151b9d87814830c036b55c7b2
seungjae9 <moseung18@gmail.com>
2020-12-03 17:55:00 +0900

    test

---
+++ v-for-remove/src/components/HelloWorld.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>
+    <h1>{{ msg }}</h1>
+    <h2>{{test}}</h2>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld',
+  data () {
+    return {
+      msg: 'Welcome to Your Vue.js App',
+      test: {
+        'A': 'asdf',
+        'B': 'fdas'
+      }
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
