5f869565d979e240bcb319314e92568fd955987e
seungjae9 <moseung18@gmail.com>
2020-12-04 01:14:26 +0900

    computed, v-for-checkbox

--- v-for-remove/src/components/HelloWorld.vue
+++ v-for-remove/src/components/HelloWorld.vue
@@ -1,7 +1,18 @@
 <template>
   <div>
-    <h1>{{ msg }}</h1>
-    <h2>{{test}}</h2>
+    <ul>
+      <li v-for="item in test" :key="item.id">
+        {{item.con}}
+        <input type="checkbox" v-bind:value="item.id" @click="con">
+      </li>
+    </ul>
+       <input type="button" class="btn btn-primary" v-on:click="OnSubmit()" name="" value="submit" />
+
+                        <span>
+
+                            {{selectedval}}
+
+                        </span>
   </div>
 </template>
 
@@ -11,10 +22,29 @@ export default {
   data () {
     return {
       msg: 'Welcome to Your Vue.js App',
-      test: {
-        'A': 'asdf',
-        'B': 'fdas'
-      }
+      test: [
+        {
+          'id': '1',
+          'con': '불라불라',
+          'isValid': true
+        },
+        {
+          'id': '2',
+          'con': '불라불라2',
+          'isValid': true
+        },
+        {
+          'id': '3',
+          'con': '불라불라3',
+          'isValid': true
+        }
+      ],
+      selected: []
+    }
+  },
+  methods: {
+    con () {
+      console.log(this.selected)
     }
   }
 }
