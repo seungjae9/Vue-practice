63fcbbe475ea7ef71d644eb6d53e32ec85c47894
seungjae9 <moseung18@gmail.com>
2020-12-08 00:27:01 +0900

    강의

--- asdfasdf/test.py
+++
@@ -1,8 +0,0 @@
-from pytube import Playlist
-
-print('재생목록 입력하삼')
-playlist = Playlist('{}'.format(input()))
-for index, video in enumerate(playlist.videos):
-    print('{} 번째 동영상 다운로드중.....'.format(index+1))
-    video.streams.first().download('video')
-print('다운로드 완료')
