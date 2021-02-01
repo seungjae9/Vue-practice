163f0e211307b20405db421ff804ba85b230b90a
seungjae9 <moseung18@gmail.com>
2021-02-01 09:30:46 +0900

    vue model test

---
+++ vue-modal/modalTest/src/components/HelloWorld.vue
@@ -0,0 +1,38 @@
+<template>
+  <div class="hello">
+    <h2>Essential Links</h2>
+    <button @click="testModal">클릭</button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld',
+  data () {
+    return {
+    }
+  },
+  methods: {
+    testModal () {
+      alert('asdf')
+    }
+  }
+}
+</script>
+
+<style scoped>
+h1, h2 {
+  font-weight: normal;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
